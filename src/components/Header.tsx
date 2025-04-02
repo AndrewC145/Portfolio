e516467c9790e0585e1ed0b94800438e42d01047
Andrew Cao <andrewcao618@gmail.com>
2025-04-02 16:21:45 -0400

    File Setup and Testing

---
+++ src/components/Header.tsx
@@ -0,0 +1,10 @@
+function Header() {
+  return (
+    <header>
+      <nav className="text-center font-bold"></nav>
+      <p className="font-bold">test</p>
+    </header>
+  );
+}
+
+export default Header;
