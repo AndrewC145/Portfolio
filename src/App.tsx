80e6f4366898d26886d02349cda6a6568e722d0f
Andrew Cao <andrewcao618@gmail.com>
2025-04-02 20:04:43 -0400

    Added Hero Section

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
 import Header from "./components/Header";
+import Hero from "./components/Hero";
 
 function App() {
-  return <Header />;
+  return (
+    <div className="bg-black text-white">
+      <Header />
+      <Hero />
+    </div>
+  );
 }
 
 export default App;
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,16 +1,24 @@
 function Header() {
   return (
-    <header className="bg-opacity-75 fixed top-0 right-0 left-0 z-50 bg-black p-4 text-white sm:px-6 md:px-8 lg:px-9 xl:px-10">
+    <header className="sticky top-0 right-0 left-0 z-50 bg-black/50 p-4 text-white sm:p-5 md:p-6">
       <nav className="flex justify-between bg-transparent font-sans font-semibold">
-        <p>Andrew</p>
-        <ul className="flex cursor-pointer gap-4 sm:gap-5 md:gap-6 lg:gap-7 xl:gap-8">
-          <li className="transition-colors duration-150 hover:text-blue-200">Home</li>
-          <li className="transition-colors duration-150 hover:text-blue-200">About</li>
-          <li className="transition-colors duration-150 hover:text-blue-200">Experience</li>
-          <li className="transition-colors duration-150 hover:text-blue-200">Projects</li>
+        <p>ANDREW</p>
+        <ul className="flex cursor-pointer gap-4 uppercase sm:gap-5 md:gap-6 lg:gap-7 xl:gap-8">
+          <ListItems />
         </ul>
       </nav>
     </header>
+  );
+}
+
+function ListItems() {
+  return (
+    <>
+      <li className="transition-colors duration-150 hover:text-blue-200">Home</li>
+      <li className="transition-colors duration-150 hover:text-blue-200">About</li>
+      <li className="transition-colors duration-150 hover:text-blue-200">Experience</li>
+      <li className="transition-colors duration-150 hover:text-blue-200">Projects</li>
+    </>
   );
 }
 
