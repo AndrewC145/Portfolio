09d08c09e3a503fbd5999f728434f86007ed2407
Andrew Cao <andrewcao618@gmail.com>
2025-04-04 19:53:24 -0400

    Added Animation to Hero Text

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,3 +1,4 @@
+import { TextFade } from "./TextFade";
 import jpVideo from "../assets/videos/IMG_0991.mp4";
 
 function Hero() {
@@ -8,12 +9,21 @@ function Hero() {
           <source src={jpVideo} type="video/mp4"></source>
         </video>
         <div className="absolute inset-0 flex items-end justify-center overflow-hidden">
-          <h1 className="mb-8 text-center font-rhd text-4xl font-bold text-[#F4F4F5] sm:mb-9 sm:text-5xl md:mb-10 md:text-6xl lg:mb-11 lg:text-7xl xl:mb-12 xl:text-8xl 2xl:mb-12 2xl:text-[150px]">
-            HEY THERE, <br /> I'M ANDREW
-          </h1>
+          <TextFade className="flex flex-col items-center justify-center" direction="up">
+            <ReuseClass text="HEY THERE," />
+            <ReuseClass text="I'M ANDREW" />
+          </TextFade>
         </div>
       </div>
     </div>
+  );
+}
+
+function ReuseClass({ text }: { text: string }) {
+  return (
+    <h1 className="mb-8 text-center font-rhd text-4xl font-bold text-[#F4F4F5] sm:mb-9 sm:text-5xl md:mb-10 md:text-6xl lg:mb-11 lg:text-7xl xl:mb-12 xl:text-8xl 2xl:mb-12 2xl:text-[150px]">
+      {text}
+    </h1>
   );
 }
 
